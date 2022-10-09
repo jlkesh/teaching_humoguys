12c0f9bc4296dbfd82e2b8cd21a47b572d1e8100
jlkesh <john.lgd65@gmail.com>
2022-10-09 13:11:02 +0500

    base initial set up

--- src/main/java/uz/jl/App.java
+++ src/main/java/uz/jl/App.java
@@ -1,4 +1,29 @@
 package uz.jl;
 
+import java.util.Random;
+
 public class App {
+    public static void main(String[] args) {
+        char tm = '\u2690';
+        System.out.println("Java" + tm);
+        String fire = "1f4A5";
+//        int codePoint = 0x1F683;
+//        System.out.println(codePoint);
+//        char highSurrogate = Character.highSurrogate(codePoint);
+//        char lowSurrogate = Character.lowSurrogate(codePoint);
+//        char[] item = new char[]{highSurrogate, lowSurrogate};
+//        System.out.println(item);
+
+        // ESC[48;5;{ID}m
+        int counter = 1;
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.print("\u001b[38;5;" + new Random().nextInt(0, 254) + "m\t" + counter);
+                counter++;
+            }
+            System.out.println();
+        }
+
+    }
 }
